2e0585a59e7c3c60b8bf7e0a8d5e377b7f2f9cd5
Jorge Diz <jorge@diz.es>
2014-02-15 20:32:40 +0100

    Add commentary explaining and/or lists

--- grammar/entities/adjectives/deff.py
+++ grammar/entities/adjectives/deff.py
@@ -37,7 +37,21 @@ adjective << (
 	| haunted
 )
 
+# 'and' captures both 'legendary creature' (juxtaposed) and 'black and red' (joined)
+
+# 'or' will capture explicit disjunctions 'black or red'
+# but since it will come after the ^, not juxtapositions (taken by 'and')
+
+# so the 'one or more' allows 'legendary black or red'
+# to be correctly interpreted as (A and (B or C))
+# it's non-intuitive, but it works
+
+# at the same time, it forces us to use ^ instead of |
+# or "target artifact, enchantment or land"
+# becomes ((A and B) or C)
+
 andadjectives << delimitedListAnd(adjective)
+
 oradjectives << delimitedListOr(adjective)
 
 adjectives << OneOrMore(andadjectives ^ oradjectives)
